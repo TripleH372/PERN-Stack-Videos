43e282fbc7fc2d9f3939ad037e41d054adfa0acc
Sid Meka <gsmekarrow@gmail.com>
2022-01-15 10:55:15 -0500

    Updated fonts in GlobalStyles.js

--- client/src/components/styled/GlobalStyles.js
+++ client/src/components/styled/GlobalStyles.js
@@ -1,6 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+
+
+* { /* Everything in here is Global (thus the asterisk)*/
+    margin: 0;
+}
+
 body {
-  margin: 0;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+  font-family: "Times New Roman", 'Roboto', 'Oxygen',
     'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
     sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -11,4 +19,7 @@ code {
   font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
     monospace;
 }
-/*We might not need this file at all. Check GlobalStyles.js*/
+
+`;
+
+export default GlobalStyle;
