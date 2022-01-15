16a914ccdc843820625de4a9a5fa1ff30fa0ab3a
Sid Meka <gsmekarrow@gmail.com>
2022-01-15 10:43:35 -0500

    Styled App is now in the return function.

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import StyledApp from "./components/styled/App.styled"; //I might use App.css
 
 function App() {
-  return <div className="App"></div>
+  return <StyledApp className="App">This is the Styled App thus so far.</StyledApp>
 }
 
 export default App;
